ad8458ba144c41e56056cc108f947d34ff5e1546
nogueira.laercio@hotmail.com <laercio.nogueira@rw3tecnologia.com>
2025-06-09 12:46:45 -0300

    config: config jest in frontend

---
+++ client/jest.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  collectCoverage: true,
+  collectCoverageFrom: ["**/*.(t|s)s"],
+  coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/jest.config.ts",
+    "/vite.config.ts",
+    "/src/app.module.ts",
+    "/main.ts",
+    "/jest.config.ts",
+  ],
+  setupFilesAfterEnv: ["<rootDir>/tests/setupTests.ts"],
+  moduleNameMapper: {
+    "@components/(.*)": "<rootDir>/src/components/$1",
+    "@store/(.*)": "<rootDir>/src/store/$1",
+    "@config/(.*)": "<rootDir>/src/config/$1",
+    "@templates/(.*)": "<rootDir>/src/templates/$1",
+    "@interfaces/(.*)": "<rootDir>/src/interfaces/$1",
+  },
+};
+
+export default config;
